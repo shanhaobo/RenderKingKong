8cb58b215f4d33957b7d162e0c4759f11b5d6efa
shanhaobo <shanhaobo@gmail.com>
2014-12-02 01:36:57 +0800

    RTTI

---
+++ Src/WIZ_Library/Include/RTTI/WizRTTIMetaDataStruct.hpp
@@ -0,0 +1,37 @@
+#ifndef __WIZ_RTTI_METADATA_STRUCT_HPP__SHANHAOBO_19800429__
+#define __WIZ_RTTI_METADATA_STRUCT_HPP__SHANHAOBO_19800429__
+
+#include "./WizRTTIMetaDataField.hpp"
+
+namespace Wiz
+{
+    namespace RTTI
+    {
+        namespace MetaData
+        {
+            namespace Struct
+            {
+                class Type : public MetaData::Field::Type
+                {
+                    typedef Type                    tThis;
+                    typedef tThis*                  tThisPtr;
+
+                    typedef MetaData::Field::Type   tSuper;
+
+                public:
+                    WIZ_DECLARE_RTTI(Type, MetaData::Field::Type);
+
+                protected:
+                    typedef tThis                   tStruct;
+                    typedef tStruct*                tStructPtr;
+
+                protected:
+                    tStructPtr                      m_ParentPtr;
+                    tFieldPtr                       m_ChildPtr;
+                };
+            } /// end of namespace Struct
+        } /// end of namespace MetaData
+    } /// end of namespace RTTI
+} /// end of namespace Wiz
+
+#endif /*__WIZ_RTTI_METADATA_STRUCT_HPP__SHANHAOBO_19800429__*/
