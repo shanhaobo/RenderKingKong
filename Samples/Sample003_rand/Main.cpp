9228217faba42b64a2248b682cbbaa3bd069d6a2
shanhaobo <shanhaobo@gmail.com>
2015-12-10 01:32:03 +0800

    sth

--- Samples/Sample003_rand/Main.cpp
+++ Samples/Sample003_rand/Main.cpp
@@ -13,6 +13,8 @@ int _tmain(int argc, _TCHAR* argv[])
     ::Wiz::R64::Type lRadius, lTheta;
     ::Wiz::R64::Type x, y;
 
+    /// 为啥要0~2500然后开方, 而不直接使用 0~50
+    /// 是因为让大半径的点更多些,毕竟 10个点分布在半径为10的圆周上 比 10个点分布在半径为50的圆周上要密
     ::Wiz::Random::Type<::Wiz::R64::Type> tRandom(0, 2500);
 
     for (::Wiz::I::Type i = 0; i < 1000; ++i)
